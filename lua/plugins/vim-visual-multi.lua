9a8a2802220ea3f5ae8309dca6e395a205c4791f
hunghnt <hunghnt@ticketbox.vn>
2026-03-05 15:12:05 +0700

    feat: update nvim

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -3,8 +3,8 @@ return {
 	branch = "master",
 	init = function()
 		vim.g.VM_maps = {
-			["Find Under"] = "<C-d>",
-			["Find Subword Under"] = "<C-d>",
+			["Find Under"] = "<C-1>",
+			["Find Subword Under"] = "<C-1>",
 			["Select All"] = "<C-S-l>",
 			["Add Cursor Down"] = "<M-C-Down>",
 			["Add Cursor Up"] = "<M-C-Up>",
